cd0c84b0c3247e28fcbbdcfc28760da14db6db59
Kavian Darvish <kavian1991@gmail.com>
2019-10-08 18:32:34 -0400

    made final changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,46 @@
-import questions from './questions.mjs'
+var computer= [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+      title :"Which internet company began life as an online bookstore called Cadabra?",
+      choices: ["Amazon","eBay","Overstock","Shopify"],
+      answer :"Amazon"
+    },
+    {
+      title:"When was the programming language C# released?",
+      choices:["1998","1999","2001","2000"],
+      answer:"2000",
+    },
+  ]
+  
+  
+  var film = [
+    {
+      title :"Which actress danced the twist with John Travolta in Pulp Fiction?",
+      choices: ["Kathy Griffin",'Uma Thurman',"Pam Grier","Bridget Fonda"],
+      answer: "Uma Thurman",
+    },
+    {
+      title:"This movie contains the quote, 'Houston, we have a problem...'",
+      choices:["The Right Stuff","Capricorn One","Apollo 13", "Marooned"],
+      answer:"Apollo 13",
+    },
+    {
+      title:"Which of these actors/actresses is NOT a part of the cast for the 2016 movie Suicide Squad?",
+      choices:["Scarlett Johansson","Jared Leto","Will Smith","Margot Robbie"],
+      answer:"Scarlett Johansson",
+    },
+  ]
+
+var questions = [ computer , film];
 let tl = new TimelineMax()
 
 
@@ -151,7 +193,7 @@ function done(){
 
 function result(e){
     if (e.path[0].textContent === answer){
-        let ding = new Audio('../sound/correct.mp3');
+        let ding = new Audio('./sound/correct.mp3');
         ding.play();
         setTimeout(()=>{
             correct.classList.remove('hide');
@@ -169,7 +211,7 @@ function result(e){
             loadCard()
         }
     } else {
-        let ding = new Audio('../sound/wrong.mp3');
+        let ding = new Audio('./sound/wrong.mp3');
         ding.play();
         setTimeout(()=>{
             wrong.classList.remove('hide');
